8aa45085127bffc46592a5df42fd7c4550d7e792
Tal Hadad <tal_hd@hotmail.com>
2023-01-16 19:27:04 +0200

    try running only on IE11 on BrowserStack

--- tests/selenium/browser-stack-caps.js
+++ tests/selenium/browser-stack-caps.js
@@ -10,26 +10,26 @@ export default [
             "sessionName" : "Parallel test 1",
         },
     },
-    {
-        name: 'IE10 (Windows 7)',
-        browserName: "IE",
-        'bstack:options' : {
-            "os": "Windows",
-            "osVersion": "7",
-            "browserVersion": "10.0",
-            "buildName" : "browserstack-build-1",
-            "sessionName" : "Parallel test 2",
-        }
-    },
-    {
-        name: 'Safari 10.1',
-        browserName: "Safari",
-        'bstack:options' : {
-            "os": "OS X",
-            "osVersion": "Sierra",
-            "browserVersion": "10.1",
-            "buildName" : "browserstack-build-1",
-            "sessionName" : "Parallel test 3",
-        },
-    },
+    // {
+    //     name: 'IE10 (Windows 7)',
+    //     browserName: "IE",
+    //     'bstack:options' : {
+    //         "os": "Windows",
+    //         "osVersion": "7",
+    //         "browserVersion": "10.0",
+    //         "buildName" : "browserstack-build-1",
+    //         "sessionName" : "Parallel test 2",
+    //     }
+    // },
+    // {
+    //     name: 'Safari 10.1',
+    //     browserName: "Safari",
+    //     'bstack:options' : {
+    //         "os": "OS X",
+    //         "osVersion": "Sierra",
+    //         "browserVersion": "10.1",
+    //         "buildName" : "browserstack-build-1",
+    //         "sessionName" : "Parallel test 3",
+    //     },
+    // },
 ];
